7b9c9eb6f51536a5ae5b0991dfcb2c3168fda160
Sara Rampazzo <sararampazzo.ph@gmail.com>
2025-02-27 00:15:01 +0100

    feat: A new react app 4

--- src/ChatApp.jsx
+++ src/ChatApp.jsx
@@ -1,24 +1,43 @@
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
+import { mutate } from "swr";
+import { useBot } from "./utils/useBot";
 
 export default function ChatApp() {
-  const [chatHistory, setChatHistory] = useState([
-    { text: "Hi!", sender: "received" },
-    { text: "Hello?", sender: "sent" },
-  ]);
+  const [chatHistory, setChatHistory] = useState([]);
 
   const [input, setInput] = useState("");
-  const [isUserTurn, setIsUserTurn] = useState(true);
 
-  const sendMessage = () => {
+  const { data } = useBot();
+
+  const sendMessage = (e) => {
+    e.preventDefault(); // funzione che serve per evitare il reload della pagina all'onSubmit del form
+
     if (!input.trim()) return;
     const newMessage = {
       text: input,
-      sender: isUserTurn ? "sent" : "received",
+      isUser: true,
     };
     setChatHistory([...chatHistory, newMessage]); // set serve per cambiare il valore di uno stato associato
-    setInput(""); // ... è uno spread operetor 
-    setIsUserTurn(!isUserTurn);
+    setInput(""); // clearing input field  // ... spread operetor
+
+    mutate("https://mocki.io/v1/731f2d9d-6a2a-4f9a-adf6-2f01de012cee").then(
+      () => {
+        if (data) {
+          const botMessage = {
+            text: data.response,
+            isUser: false,
+          };
+          setChatHistory((prevMessage) => [...prevMessage, botMessage]);
+        }
+      }
+    ); // serve per richiamare il fetch
   };
+
+  const lastMessageRef = useRef(null);
+
+  useEffect(() => {
+    lastMessageRef.current.scrollIntoView();
+  }, [chatHistory]);
 
   return (
     <div className="p-4 max-w-md mx-auto bg-white rounded-lg shadow-md">
@@ -27,7 +46,7 @@ export default function ChatApp() {
           <div
             key={index} // serve per evitare errori non bloccanti in console
             className={`p-2 my-1 rounded-lg shadow-md max-w-[75%] ${
-              msg.sender === "sent"
+              msg.isUser
                 ? "bg-purple-500 text-white ml-auto"
                 : "bg-white text-black mr-auto"
             }`} // msg = oggetto , sender è la chiave , sent è il valore della chiave
@@ -35,22 +54,23 @@ export default function ChatApp() {
             {msg.text}
           </div>
         ))}
+
+        <div ref={lastMessageRef}></div>
       </div>
-      <div className="mt-2 flex shadow-md rounded-lg overflow-hidden">
+      <form
+        className="mt-2 flex shadow-md rounded-lg overflow-hidden"
+        onSubmit={(e) => sendMessage(e)} // serve per inviare il form a cui passiamo un event
+      >
         <input
           type="text"
           className="flex-1 p-2 border-none focus:outline-none"
           value={input}
           onChange={(e) => setInput(e.target.value)} // (e) = event
-          onKeyPress={(e) => e.key === "Enter" && sendMessage()}
         />
-        <button
-          onClick={sendMessage}
-          className="px-4 py-2 bg-purple-500 text-white"
-        >
-          Invia
+        <button type="submit" className="px-4 py-2 bg-purple-500 text-white">
+          Send
         </button>
-      </div>
+      </form>
     </div>
   );
 }
